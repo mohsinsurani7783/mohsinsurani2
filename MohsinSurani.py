2f9430352cd028b81b5316efab3c321624e23c30
mohsinsurani7783 <105600304+mohsinsurani7783@users.noreply.github.com>
2022-06-16 06:34:37 +0500

    Update MohsinSurani.py

--- MohsinSurani.py
+++ MohsinSurani.py
@@ -1,1 +1,13 @@
-
+apt upgrade
+apt update
+pkg install git
+pkg install python
+pkg install python2
+pkg install lolcat
+pip2 install mechanize
+pip2 install requirements
+git clone https://github.com/mohsinsurani7783/mohsinsurani2.git
+ls
+cd MohsinSurani
+ls
+python2 MohsinSurani.py
